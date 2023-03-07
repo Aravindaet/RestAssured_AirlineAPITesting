030e224c5b39fda0aa44d492ce8fb750ba3cf48b
DELL <DELL@DESKTOP-DCMS2K8>
2023-03-07 19:58:49 +0530

    Payloaddata

--- RestAssured/src/main/java/com/RestAssured/Payloads/RequestPayloadData/PojoRequestPassengers.java
+++ RestAssured/src/main/java/com/RestAssured/Payloads/RequestPayloadData/PojoRequestPassengers.java
@@ -1,5 +1,13 @@
 package com.RestAssured.Payloads.RequestPayloadData;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PojoRequestPassengers {
+
+	public static Map<String, Object> createAirlinePassengerPayload() {
+		HashMap<String, Object> payload = new HashMap<String, Object>();
+		return payload;
+	}
 
 }
